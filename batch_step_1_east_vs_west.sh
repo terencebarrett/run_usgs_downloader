c89fa787d940253f40f392ba7e23f3d8437609eb
tcbarret <tcbarret@uvm.edu>
2022-01-24 11:11:28 -0500

    Add full VACC paths to script calls

--- batch_step_1_east_vs_west.sh
+++ batch_step_1_east_vs_west.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Rscript run_step_1.R -s DE -c 1000
-Rscript run_step_1.R -s CO -c 1000 --local_entwine_index
+Rscript "$HOME/code/run_usgs_downloader/run_step_1.R" -s DE -c 1000
+Rscript "$HOME/code/run_usgs_downloader/run_step_1.R" -s CO -c 1000 --local_entwine_index
