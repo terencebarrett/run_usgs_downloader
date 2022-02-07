a2298f5542cacf6fda708d7ecc7fc7c506d379eb
tcbarret <tcbarret@uvm.edu>
2022-02-07 12:01:08 -0500

    Update for slurming step 1 for western states

---
+++ batch_step_1_western_states.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+Rscript run_step_1.R -s AK -c 1000
+Rscript run_step_1.R -s AS -c 1000 --local_entwine_index
+Rscript run_step_1.R -s AZ -c 1000 --local_entwine_index
+Rscript run_step_1.R -s CA -c 1000 --local_entwine_index
+Rscript run_step_1.R -s CO -c 1000 --local_entwine_index
+Rscript run_step_1.R -s GU -c 1000 --local_entwine_index
+Rscript run_step_1.R -s HI -c 1000 --local_entwine_index
+Rscript run_step_1.R -s ID -c 1000 --local_entwine_index
+Rscript run_step_1.R -s KS -c 1000 --local_entwine_index
+Rscript run_step_1.R -s MP -c 1000 --local_entwine_index
+Rscript run_step_1.R -s MT -c 1000 --local_entwine_index
+Rscript run_step_1.R -s ND -c 1000 --local_entwine_index
+Rscript run_step_1.R -s NE -c 1000 --local_entwine_index
+Rscript run_step_1.R -s NM -c 1000 --local_entwine_index
+Rscript run_step_1.R -s NV -c 1000 --local_entwine_index
+Rscript run_step_1.R -s OK -c 1000 --local_entwine_index
+Rscript run_step_1.R -s OR -c 1000 --local_entwine_index
+Rscript run_step_1.R -s SD -c 1000 --local_entwine_index
+Rscript run_step_1.R -s TX -c 1000 --local_entwine_index
+Rscript run_step_1.R -s UT -c 1000 --local_entwine_index
+Rscript run_step_1.R -s WA -c 1000 --local_entwine_index
+Rscript run_step_1.R -s WY -c 1000 --local_entwine_index
