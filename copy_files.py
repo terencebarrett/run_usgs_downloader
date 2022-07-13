f5b4cad80ebdf6554ac13cc6596447a4913147d7
tcbarret <tcbarret@uvm.edu>
2022-07-13 16:52:02 -0400

    Add bool option to choose copy-only-first-match or copy-all-matches

--- copy_files.py
+++ copy_files.py
@@ -2,58 +2,42 @@ from pathlib import Path
 
 from shutil import copy2
 
-input_folder = '/gpfs2/scratch/tcbarret/zip_processing/Park/2019/Products/LAS'
-output_folder = '/gpfs2/scratch/tcbarret/zip_processing/Park_selection'
-keyword_list = ['CWCB_PARK_04331',
-                'CWCB_PARK_04332',
-                'CWCB_PARK_04333',
-                'CWCB_PARK_04334',
-                'CWCB_PARK_04335',
-                'CWCB_PARK_04385',
-                'CWCB_PARK_04386',
-                'CWCB_PARK_04387',
-                'CWCB_PARK_04388',
-                'CWCB_PARK_04389',
-                'CWCB_PARK_04439',
-                'CWCB_PARK_04440',
-                'CWCB_PARK_04492',
-                'CWCB_PARK_04493',
-                'CWCB_PARK_04543',
-                'CWCB_PARK_04544',
-                'CWCB_PARK_04594',
-                'CWCB_PARK_04595',
-                'CWCB_PARK_04645',
-                'CWCB_PARK_04646',
-                'CWCB_PARK_04696',
-                'CWCB_PARK_04697',
-                'CWCB_PARK_04747',
-                'CWCB_PARK_04748',
-                'CWCB_PARK_04797',
-                'CWCB_PARK_04798',
-                'CWCB_PARK_04847',
-                'CWCB_PARK_04848',
-                'CWCB_PARK_04898',
-                'CWCB_PARK_04899',
-                'CWCB_PARK_04950',
-                'CWCB_PARK_04951',
-                'CWCB_PARK_05002',
-                'CWCB_PARK_05003',
-                'CWCB_PARK_05054',
-                'CWCB_PARK_05055',
-                'CWCB_PARK_05106',
-                'CWCB_PARK_05107',
-                'CWCB_PARK_05156',
-                'CWCB_PARK_05157',
-                'CWCB_PARK_05204',
-                'CWCB_PARK_05205',
-                'CWCB_PARK_05253',
-                'CWCB_PARK_05254']
+input_folder = '/gpfs2/scratch/tcbarret/completed_downloader_runs/noaa_retrievals_20220628'
+output_folder = '/gpfs2/scratch/tcbarret/completed_dataprep_runs/quality_check_20220629_noaa_as_downloaded__reporting/05_problem_laz_files'
+copy_only_first_match = False  # Faster, but doesn't ensure uniqueness
+keyword_list = [
+    '522_236617012010854',
+    '522_236617176010854',
+    '522_249673342010854',
+    '522_249673383010854',
+    '522_259177984010854',
+    '522_259085016010854',
+    '522_259085032010854',
+    '540_153666060010854',
+    '540_174758516020004',
+    '540_174758537020004',
+    '540_228183329010854',
+    '540_259084971010854',
+    '531_221353737010661',
+    '531_247057117010661',
+    '522_249676358010854',
+    '540_197125538010854',
+    '540_259118863010854',
+    '521_168988973010661',
+    '521_222442739010661',
+    '521_247063050010661',
+    '521_247063054010661',
+    '1061_47885274020004',
+    '531_155531778010854',
+    '531_155531785010854',
+    '531_252281777010854',
+]
 
 input_path = Path(input_folder)
 output_path = Path(output_folder)
 output_path.mkdir()
 
-input_files = sorted(input_path.glob('*'))
+input_files = sorted(input_path.rglob('*'))
 
 for keyword in keyword_list:
     print(f'Keyword: {keyword}')
@@ -61,6 +45,5 @@ for keyword in keyword_list:
         if keyword in str(input_file):
             print(f'Copying: {input_file.name}')
             copy2(input_file, output_path)
-            break
-
-
+            if copy_only_first_match:
+                break
